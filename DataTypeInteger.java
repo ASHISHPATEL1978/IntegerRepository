909286c1cba57c2c5f10014f6d51e54d2faa78ca
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 23:16:47 +0000

    Integer Data Type Commt

---
+++ DataTypeInteger.java
@@ -0,0 +1,19 @@
+package datatypeinteger;
+
+public class DataTypeInteger
+{
+    public static void main ( String[] args )
+    {
+        int A = 20;
+        int B = 4;
+
+        System.out.println ( A + B );
+        System.out.println ( A - B );
+        System.out.println ( A * B );
+        System.out.println ( A / B );
+        System.out.println ( A++ );
+        System.out.println ( B-- );
+        System.out.println ( ++A + --B );
+        System.out.println ( ++A - --B );
+    }
+}
